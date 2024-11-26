fc7dc4e2b0b31e4390bd9a19c80b81701ad19e71
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-11-26 22:41:38 +0000

    asd123

--- src/js/views/EditContact.js
+++ src/js/views/EditContact.js
@@ -4,6 +4,28 @@ import { Link } from "react-router-dom "
 const EditContact = () => {
 	return (
 		<>
+			<div className="container bg-white rounded p-3">
+				<div className="mb-3">
+					<label className="form-label">Nombre</label>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="mb-3">
+					<label className="form-label">Telefono</label>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="mb-3">
+					<label className="form-label">Email</label>
+					<input type="email" className="form-control" />
+				</div>
+				<div className="mb-3">
+					<label className="form-label">Direccion</label>
+					<input type="text" className="form-control" />
+				</div>
+
+				<Link to="/">
+					<button className="btn btn-primary">Back home</button>
+				</Link>
+			</div>
 		</>
 	)
 }
