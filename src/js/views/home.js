ae5bcdd1fcb8360b5b007ec162e16dcad8243ae2
Felipe Coste <32721311+felipecoste@users.noreply.github.com>
2024-12-02 00:27:05 +0000

    se empieza a crear la vista de home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,17 +3,33 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Navbar } from "../component/navbar";
 
-export const Home = () => (
-	<>
-		<Navbar />
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				Necesito empezar este proyecto ya!
-			</a>
-		</div>
-	</>
-);
+
+const API_BASE_URL = 'https://playground.4geeks.com/contact/agendas/'
+
+export const Home = () => {
+	
+
+	const getUsaerAgenda = async () => {
+		try {
+			const response = await fetch(`${API_BASE_URL}/Felipe`)
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+
+	return (
+		<>
+			<Navbar />
+			<div className="text-center mt-5">
+				<h1>Hello Rigo!</h1>
+				<p>
+					<img src={rigoImage} />
+				</p>
+				<a href="#" className="btn btn-success">
+					Necesito empezar este proyecto ya!
+				</a>
+			</div>
+		</>
+	);
+}
